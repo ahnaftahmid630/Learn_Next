94fb325470b55be7b35c8909b8266f0d7c6a57d3
Tahmid <atahmid253@gmail.com>
2026-08-12 00:02:36 +0600

    add all things

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link"
 import { Button } from "@/components/ui/button";
+
 import {ArrowRight} from "lucide-react"
 import signUp from "./signUp/page";
 
@@ -20,6 +21,37 @@ export default function Home() {
               </div>
               </div>
               
+        </section>
+        {/* another section starts */}
+        <section className="border-t bg-white py-16">
+          <div className="container mx-auto px-4">
+            <div className="mx-auto  max-w-6xl">
+              {/* tabs are here */}
+              <div className="felx gap-2 justify-center items-center mx-auto mb-8">
+                  <Button>Organize Application</Button>
+                  <Button>Get Hired</Button>
+                  <Button>Manage Boards</Button>
+              </div >
+              {/* imges */}
+              <div className="relative mx-auto max-w-5xl overflow-hidden rounded-lg border border-gray-200 shadow-xl">
+                  <Image src="/hero-images/hero1.png" 
+                          alt="hero image 1"
+                          width={1200}
+                          height={800}></Image>
+
+                          <Image src="/hero-images/hero2.png" 
+                          alt="hero image 1"
+                          width={1200}
+                          height={800}></Image>
+
+                          <Image src="/hero-images/hero3.png" 
+                          alt="hero image 1"
+                          width={1200}
+                          height={800}></Image>
+
+              </div>
+            </div>
+          </div>
         </section>
 
       </main>
